0bbe0cf19d9fe67e3a966fd8f0fb89325fc2e7d9
Matheus <dinizmaths@gmail.com>
2023-06-19 07:07:11 -0300

    added index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,31 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import Message from './components/Message';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/")
-      .then((response) => {
-        const data = response.data
-
-        setMessage(data["message"])
-      })
-      .catch((err) => {
-
-      })
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="mt-3 justify-content-center align-items-center mx-auto" style={{"width": "70vw", "backgroundColor": "#f5f5f5"}}>
+      </div>
 
-        <Message message={message}/>
-      </header>
+      <h2 className="text-center text-white bg-success card mb-1 p-2">
+        Players Manager
+      </h2>
+
+      <h6 className="card text-center text-white bg-success card mb-2 p-2">
+        Player Information
+      </h6>
+
+      <div className="card-body text-center">
+        <h5 className="card text-center text-white bg-dark p-2">
+          Player Register
+        </h5>
+        
+        <span>
+
+        </span>
+      </div>
     </div>
   );
 }
